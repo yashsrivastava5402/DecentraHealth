775124424221992ff434cfde6bd455298c4f2aa3
Yash Srivastava <66536990+yashsrivastava5402@users.noreply.github.com>
2022-07-15 00:30:15 +0530

    Update ViewReports.js

--- frontend/src/components/ViewReports.js
+++ frontend/src/components/ViewReports.js
@@ -11,7 +11,7 @@ export default function ViewReports() {
     console.log(state);
     const [files, setFiles] = useState([]);
     useEffect(() => {
-        axios.post('http://localhost:8000/viewFiles', {aadhar: state}).then((response) => {
+        axios.post('https://decentrahealth-server.herokuapp.com/viewFiles', {aadhar: state}).then((response) => {
             console.log(response.data);
                 setFiles((prevState) => {
                     return [...prevState,...response.data];
