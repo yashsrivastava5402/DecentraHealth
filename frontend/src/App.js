81995d77ad67dfd226d487047a47f8290770980d
Aaditya Mishra <codewithaadi31@gmail.com>
2023-03-13 22:08:03 +0530

    Naming Change.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,8 @@
-import Home from "./components/Home";
+import React from "react";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+
+//Components
+import Home from "./components/Home/Home";
 import DoctorLogin from "./components/DoctorLogin";
 import HospitalSignupPage from "./components/HospitalSignupPage";
 import HospitalLogin from "./components/HospitalLogin";
@@ -6,47 +10,45 @@ import Admin from "./components/Admin";
 import PatientLogin from "./components/PatientLogin";
 import Patient from "./components/Patient";
 import PatientPage from "./components/PatientPage";
-import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
-import React from "react";
 import FileUpload from "./components/FileUpload";
 import FileDownload from "./components/FileDownload";
 import PatientPageHospital from "./components/PatientPageHospital"
 import PatientPageDoctor from "./components/PatientPageDoctor";
 import Doctor from "./components/Doctor";
-import Appbar from "./components/appbar/Appbar";
+import Appbar from "./components/Navbar/Appbar";
 import ViewReports from "./components/ViewReports";
 import Creators from "./components/Creators";
 import InputSymptoms from "./components/InputSymptoms";
 import RecommendedDocs from "./components/RecommendedDocs";
+
 function App() {
-   { document.body.style.backgroundImage = "url('https://www.transparenttextures.com/patterns/black-thread-light.png')"; }
+  //  { document.body.style.backgroundImage = "url('https://www.transparenttextures.com/patterns/black-thread-light.png')"; }
   return (
-    <div>
-    <BrowserRouter>
-    <Appbar/>
-    <Routes>
-      <Route path="/home" element={<Home />} /> 
-      <Route path="/DoctorLogin" element={<DoctorLogin />} /> 
-      <Route path="/HospitalSignupPage" element={<HospitalSignupPage />} /> 
-      <Route path="/HospitalLogin" element={<HospitalLogin />} /> 
-      <Route path="/Admin/:id" element={<Admin />} />
-      <Route path="/PatientLogin" element={<PatientLogin />} />
-      <Route path="/Patient/:phone" element={<Patient />} />
-      <Route path="/PatientPage/:id" element={<PatientPage />} />
-      <Route path="/fileUpload/:aadhar" element={<FileUpload />} />
-      <Route path="/FileDownload" element={<FileDownload />} />
-      <Route path="/PatientPageDoctor/:aadhar" element={<PatientPageDoctor />} />
-      <Route path="/PatientPageHospital/:aadhar" element={<PatientPageHospital />} />
-      <Route path="/Doctor" element={<Doctor />} />
-      <Route path="/InputSymptoms" element={<InputSymptoms/>} />
-      <Route path="/ViewReports" element={<ViewReports />} />
-      <Route path="/RecommendedDocs" element={<RecommendedDocs />} />
-      <Route path="/" element={<Navigate replace to="/home" />} />   
-      <Route path="/Creators" element={<Creators />} /> 
-    </Routes>
-  </BrowserRouter>
-  
-  </div>
+    <>
+      <BrowserRouter>
+        <Appbar />
+        <Routes>
+          <Route path="/home" element={<Home />} />
+          <Route path="/DoctorLogin" element={<DoctorLogin />} />
+          <Route path="/HospitalSignupPage" element={<HospitalSignupPage />} />
+          <Route path="/HospitalLogin" element={<HospitalLogin />} />
+          <Route path="/Admin/:id" element={<Admin />} />
+          <Route path="/PatientLogin" element={<PatientLogin />} />
+          <Route path="/Patient/:phone" element={<Patient />} />
+          <Route path="/PatientPage/:id" element={<PatientPage />} />
+          <Route path="/fileUpload/:aadhar" element={<FileUpload />} />
+          <Route path="/FileDownload" element={<FileDownload />} />
+          <Route path="/PatientPageDoctor/:aadhar" element={<PatientPageDoctor />} />
+          <Route path="/PatientPageHospital/:aadhar" element={<PatientPageHospital />} />
+          <Route path="/Doctor" element={<Doctor />} />
+          <Route path="/InputSymptoms" element={<InputSymptoms />} />
+          <Route path="/ViewReports" element={<ViewReports />} />
+          <Route path="/RecommendedDocs" element={<RecommendedDocs />} />
+          <Route path="/" element={<Navigate replace to="/home" />} />
+          <Route path="/Creators" element={<Creators />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
--- frontend/src/components/Home/Home.jsx
+++ frontend/src/components/Home/Home.jsx
@@ -1,9 +1,7 @@
 import React from 'react'
-import { useNavigate } from "react-router";
 import Banner from './Banner';
 
 function Home() {
-  const navigate = useNavigate();
   return (
     <>
         <Banner />
