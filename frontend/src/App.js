6bcbf7aa4d361d141377433320cea8c0abadf6b4
Aaditya Mishra <codewithaadi31@gmail.com>
2023-03-18 18:56:24 +0530

    Removed Chart

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -24,7 +24,7 @@ import Creators from "./components/Creators";
 import InputSymptoms from './components/PatientPage/InputSymptoms';
 import PatientIntroPage from "./components/PatientPage/PatientIntroPage";
 import RecommendedDocs from "./components/RecommendedDocs";
-import Chartrender from "./components/utils/chart/Chartrender"
+
 //Material UI
 import { Box } from '@mui/material';
 import PatientInsight from "./components/PatientPage/PatientInsight";
@@ -60,7 +60,6 @@ function App() {
             <Route path="/RecommendedDocs" element={<RecommendedDocs />} />
             <Route path="/" element={<Navigate replace to="/home" />} />
             <Route path="/Creators" element={<Creators />} />
-            <Route path="/Chartrender" element={<Chartrender />} />
           </Routes>
           </Box>
       </BrowserRouter>
