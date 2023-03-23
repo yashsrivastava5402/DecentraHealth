68cf833f09d3865929803a0c13549fe7d2544664
Aaditya Mishra <codewithaadi31@gmail.com>
2023-03-23 08:47:04 +0530

    Hospital:Admin Also Changed, Project Completed!!!

--- frontend/src/components/HospitalPage/AddDoctor.jsx
+++ frontend/src/components/HospitalPage/AddDoctor.jsx
@@ -1,11 +1,17 @@
 import * as React from 'react';
 import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
-import {TextField, Button,Dialog } from '@material-ui/core'
+import { TextField, Button, Dialog } from '@material-ui/core'
+import { Box,styled } from '@mui/material';
 
-export default function AddDoctor({handleChange,values,onClick,submitted}) {
+const DialogBox = styled(DialogContent)`
+    &>div:{
+        marginTop: 20px;
+    }
+`
+
+export default function AddDoctor({ handleChange, values, onClick, submitted }) {
     const [open, setOpen] = React.useState(false);
 
     const handleClickOpen = () => {
@@ -17,35 +23,30 @@ export default function AddDoctor({handleChange,values,onClick,submitted}) {
     };
 
     return (
-    <div>
-    <Button size="large" type='submit' variant="contained" fullWidth style={{ height: "60px", width: "150px", alignItems: "center", backgroundColor: "#af9c4d", color: "white", justifyContent: "center", position: 'absolute', top: "0px", right: "-170px" }} onClick={handleClickOpen}> Add Doctor </Button>
-    <Dialog open={open} onClose={handleClose}>
-    <DialogTitle>Registration Form</DialogTitle>
-    <DialogContent>
-    <DialogContentText>
+        <Box style={{ margin: '0% 0% 0% 8%' }}>
+            <Button size="large" type='submit' variant="contained" onClick={handleClickOpen} > Add Doctor </Button>
+            <Dialog open={open} onClose={handleClose}>
+                <DialogTitle>Registration Form</DialogTitle>
+                <DialogBox>
+                    <TextField onChange={handleChange} value={values.Name} id="Name" class="form-field" type="text" placeholder="Doctor Name" name="Name" fullWidth /> {submitted && !values.Name ? <span id="Name-error">Please enter Doctor Name</span> : null}
 
-    </DialogContentText>
 
-    <TextField onChange={handleChange} value={values.Name} id="Name" class="form-field" type="text" placeholder="Doctor Name" name="Name" fullWidth /> {submitted && !values.Name ? <span id="Name-error">Please enter Doctor Name</span> : null}
-    <br />
+                    <TextField onChange={handleChange} value={values.UPRN} id="URPN" class="form-field" type="text" placeholder="UPRN Number" name="UPRN" fullWidth /> {submitted && !values.Name ? <span id="Name-error">Please enter UPRN Number</span> : null}
 
-    <TextField onChange={handleChange} value={values.UPRN} id="URPN" class="form-field" type="text" placeholder="UPRN Number" name="UPRN" fullWidth /> {submitted && !values.Name ? <span id="Name-error">Please enter UPRN Number</span> : null}
-    <br />
-    <TextField onChange={handleChange} value={values.speciality} id="speciality" class="form-field" type="text" placeholder="speciality" name="speciality" fullWidth /> {submitted && !values.Name ? <span id="Name-error">Please enter speciality</span> : null}
-    <br />
-    <Button size="large" type='submit' variant="contained"
-            fullWidth style={{
-              height: "60px", width: "150px", margin: "70px", alignItems: "center",
-              backgroundColor: "#013220",
-              color: "white",
-              justifyContent: "center"
-            }} onClick={onClick}>Add Doctor</Button>
-    <br />
+                    <TextField onChange={handleChange} value={values.speciality} id="speciality" class="form-field" type="text" placeholder="speciality" name="speciality" fullWidth /> {submitted && !values.Name ? <span id="Name-error">Please enter speciality</span> : null}
 
-    </DialogContent>
-    <DialogActions>
-    </DialogActions>
-    </Dialog>
-    </div>
+                    <Button size="large" type='submit' variant="contained"
+                        fullWidth style={{
+                            height: "60px", width: "150px", margin:'25px',alignItems: "center",
+                            backgroundColor: "#013220",
+                            color: "white",
+                            justifyContent: "center"
+                        }} onClick={onClick}>Add Doctor</Button>
+
+                </DialogBox>
+                <DialogActions>
+                </DialogActions>
+            </Dialog>
+        </Box>
     );
 }
