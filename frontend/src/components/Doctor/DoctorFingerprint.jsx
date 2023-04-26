bf7c45b907192a74f2aa10363789c7daef05919d
Aaditya Mishra <codewithaadi31@gmail.com>
2023-04-27 00:00:09 +0530

    Done Some changes.

--- frontend/src/components/Doctor/DoctorFingerprint.jsx
+++ frontend/src/components/Doctor/DoctorFingerprint.jsx
@@ -4,6 +4,7 @@ import { useEffect } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom'
 import { useState } from 'react';
 import axios from 'axios';
+import FullAccess from './AccessModes/FullAccess';
 
 //MUI
 import { Box, Typography, styled } from "@mui/material";
@@ -34,40 +35,63 @@ const Component = styled(Box)`
 export default function DoctorFingerprint() {
   const navigate = useNavigate();
   const { state } = useLocation();
-  const [user,setUser] = useState([]);
 
-  const getUser = async ()=>{
+  const [login, setLogin] = useState(false);
+
+  const [user, setUser] = useState([]);
+
+  const getUser = async () => {
     console.log(state.doctorId)
-    axios.post('http://localhost:8000/level', {doctorId:state.doctorId})
-    .then(function (response) {
-      if(response.status === 200){
-        navigate("/PatientIntroPage", { state: { Name: response.data.name, Aadhar: response.data.aadhar, Age: response.data.age, Gender: response.data.gender } });
-      }
-    });
+    axios.post('http://localhost:8000/level', { doctorId: state.doctorId })
+      .then(function (response) {
+        if (response.status === 200) {
+          setLogin(true);
+        }
+      });
     // console.log(data);
     // setUser(data);    
   }
 
-  useEffect(()=>{
-      getUser();
-  },[]);
+  useEffect(() => {
+    getUser();
+  }, []);
 
   return (
     <>
-      <ParentComponent>
-        <Component>
-          {/* <Title> DecentraHealth </Title> */}
-          {/* <img src="/patientLoginIntro.jpg" alt="patient_login_intro" style={{ width: 400, height: 300 }} /> */}
-          <Typography style={{ color: '#1AAE9F', fontSize: 28, fontWeight: 600,marginBottom:30 }}>Patient Login</Typography>
-          <Typography variant='h6'>Use your Biometrics to verify Identity</Typography>
-          {/* <img src="/fingerprint.png" alt="fingerprint" style={{height: 60,width:60,marginTop:10}} /> */}
-          <div class="scanner">
-            <div class="fingarprint">
+      {
+        login === false ? <ParentComponent>
+          <Component>
+            {/* <Title> DecentraHealth </Title> */}
+            {/* <img src="/patientLoginIntro.jpg" alt="patient_login_intro" style={{ width: 400, height: 300 }} /> */}
+            <Typography style={{ color: '#1AAE9F', fontSize: 28, fontWeight: 600, marginBottom: 30 }}>Patient Login</Typography>
+            <Typography variant='h6'>Use your Biometrics to verify Identity</Typography>
+            {/* <img src="/fingerprint.png" alt="fingerprint" style={{height: 60,width:60,marginTop:10}} /> */}
+            <div class="scanner">
+              <div class="fingarprint">
+              </div>
+              <h3>Scanning ....</h3>
             </div>
-            <h3>Scanning ....</h3>
-          </div>
-        </Component>
-      </ParentComponent>
+          </Component>
+        </ParentComponent> :
+          <Tablecontainer>
+            <Table sx={{ minWidth: 650 }} aria-label="simple table">
+              <TableHead>
+                <Tablerow>
+                  <TableCell>Patient's Name</TableCell>
+                  <TableCell>Aadhar</TableCell>
+                  <TableCell>Insights</TableCell>
+                  <TableCell>Reports</TableCell>
+                </Tablerow>
+              </TableHead>
+              <TableBody>
+                {state.fullPatients.map((patient) => (
+                  <FullAccess aadhar={patient && patient.Aadhar} name={patient && patient.Name} />
+                ))}
+              </TableBody>
+            </Table>
+          </Tablecontainer>
+      }
+
     </>
   )
 }
